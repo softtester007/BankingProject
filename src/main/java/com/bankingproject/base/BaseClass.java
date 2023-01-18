7b0b9e665e7ad8974968f5e7229b645625499ef0
Deepak Pakhare <deepakpakhare14@gmail.com>
2023-01-18 21:44:57 +0530

    My First Commit

---
+++ src/main/java/com/bankingproject/base/BaseClass.java
@@ -0,0 +1,14 @@
+package com.bankingproject.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://demo.guru99.com/V1/index.php");
+	}
+}
